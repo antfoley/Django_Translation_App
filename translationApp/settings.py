eca711518b9e1f300639a822fe98c51714aa3324
Anthony Foley <anthony.foley@surewash.com>
2024-10-03 10:40:37 +0100

    more pylint changes in translationApp

--- translationApp/settings.py
+++ translationApp/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-GOOGLE_CLOUD_CREDENTIALS_PATH =  os.path.join(BASE_DIR, 
+GOOGLE_CLOUD_CREDENTIALS_PATH =  os.path.join(BASE_DIR,
                                               'translationApp',
                                               'booming-post-404017-49309d69296e.json')
 GOOGLE_PROJECT_ID = "booming-post-404017"
