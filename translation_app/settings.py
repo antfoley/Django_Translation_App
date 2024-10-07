92e6aa5be662ed2c8d63effe0cf6615272ffeb47
Anthony Foley <anthony.foley@surewash.com>
2024-10-07 14:46:06 +0100

    changes to aws.yml and settings.py

--- translation_app/settings.py
+++ translation_app/settings.py
@@ -36,7 +36,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = 'django-insecure-%ru=@2zb=_mgnwb%wr*yfq==^x-5gw_@^f@u*mggne^b@q6p1^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['3.253.56.19', '127.0.0.1', 'localhost']
 
